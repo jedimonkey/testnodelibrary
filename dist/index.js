b45213675210abee6a93294e15d2c3d588760d60
Marcus Collier <marcus@mjcollier.id.au>
2016-11-24 21:22:17 +0800

    Added compiled version

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _safe = require('colors/safe');
+
+var _safe2 = _interopRequireDefault(_safe);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log(_safe2.default.blue('Hello world'));
